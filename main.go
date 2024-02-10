1182edb4fa1781957965f068d935a8fbc8daadb1
Gabriel De La Rosa Paniagua <gabriel.delarosa@solvex.com.do>
2024-02-10 19:30:45 -0400

    feat: Smallest Difference in goals team exercise done

--- main.go
+++ main.go
@@ -9,6 +9,51 @@ import (
 )
 
 func main() {
+	file, err := os.Open("data/football.dat")
+	if err != nil {
+		fmt.Println("There was an error opening the file: ", err)
+		return
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var teamWithSmallestDifference string
+	var smallestDifference int = 9999
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		fields := strings.Fields(line)
+
+		if len(fields) < 10 || fields[0] == "Team" {
+			continue
+		}
+
+		team := fields[1]
+		goalsFor, _ := strconv.Atoi(fields[6])
+		goalsAgainst, _ := strconv.Atoi(fields[8])
+
+		goalDifference := abs(goalsFor - goalsAgainst)
+
+		if goalDifference < smallestDifference {
+			smallestDifference = goalDifference
+			teamWithSmallestDifference = team
+		}
+	}
+
+	fmt.Println("Team with smallest difference: ", teamWithSmallestDifference)
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func main2() {
 	file, err := os.Open("data/weather.dat")
 	if err != nil {
 		fmt.Println("There was an error opening the file: ", err)
