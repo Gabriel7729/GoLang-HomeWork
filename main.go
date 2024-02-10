de7b0b92fd674dff536225e00ac773c6018ad8e4
Gabriel De La Rosa Paniagua <gabriel.delarosa@solvex.com.do>
2024-02-10 19:17:24 -0400

    feat: First part of the exercise weather app console

--- main.go
+++ main.go
@@ -1,9 +1,58 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Print("Hello World")
+	file, err := os.Open("data/weather.dat")
+	if err != nil {
+		fmt.Println("There was an error opening the file: ", err)
+		return
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var minSpread int = 9999
+	var minSpreadDay int = 0
+
+	for scanner.Scan() {
+
+		line := scanner.Text()
+
+		fields := strings.Fields(line)
+
+		if len(fields) < 3 || fields[0] == "Dy" {
+			continue
+		}
+
+		day, err := strconv.Atoi(fields[0])
+		if err != nil {
+			continue
+		}
+
+		maxTemp, err := strconv.Atoi(fields[1])
+		if err != nil {
+			continue
+		}
+
+		minTemp, err := strconv.Atoi(fields[2])
+		if err != nil {
+			continue
+		}
+
+		spread := maxTemp - minTemp
+		if spread < minSpread {
+			minSpread = spread
+			minSpreadDay = day
+		}
+	}
+
+	fmt.Println("Day with minimum temperature spread: ", minSpreadDay)
 }
